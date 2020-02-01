5d1482483a6fabf6306eb19479ce17a78b6ebf5b
Magor Balassy <magor.balassy@gmail.com>
2020-02-01 10:18:47 +0100

    Added multi-threaded reading as well,  of the file list to be copied.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A tool to copy files in a multi-threaded fashion.  
 Useful when copying large amount of small files to a disk mounted over network (NAS --- NAS, local <---> NAS)) for ex.  
+Will read the filelist and copy the files in multi-threaded manner.
 
 Performance gain of up to 10X vs `rsync` when copying a with 250.000 files to a NAS mount, in a tree structure of 3 levels of folders ending each in one file.
 
--- mtcopy.py
+++ mtcopy.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-import argparse, logging,os, shutil, sys
+import argparse
+import logging
+import os
+import shutil
+import sys
 
 from threading import Thread
 from queue import Queue
@@ -11,15 +15,18 @@ ERROR = False
 
 
 def copier(file_q, thread_nr):
+    global source_folder
+    global dest_folder
     while not file_q.empty():
         data = file_q.get()
-        source_file = data[1]
+        source_file = os.path.abspath(data[1])
         item_nr = data[0]
         shortname = os.path.basename(source_file)
+        print (data, source_file,source_folder)
         extra_path = source_file.split(source_folder)[1].strip('/')
         extra_folders = extra_path.split(shortname)[0].strip('/')
         if shortname != extra_path:
-            dest_file = dest_folder +'/' + extra_path
+            dest_file = dest_folder + '/' + extra_path
             os.makedirs(dest_folder + '/' + extra_folders, exist_ok=True)
         else:
             dest_file = dest_folder + '/' + shortname
@@ -28,7 +35,8 @@ def copier(file_q, thread_nr):
         except:
             logging.error("Thread %d - Copy of %s failed. Dest : %s" %
                           (thread_nr, source_file, dest_file))
-            logging.error('Shortname %s; extra path :%s; extra folders:%s' % (shortname, extra_path, extra_folders))
+            logging.error('Shortname %s; extra path :%s; extra folders:%s' % (
+                shortname, extra_path, extra_folders))
             file_q.task_done()
             ERROR = True
             return False
@@ -38,6 +46,17 @@ def copier(file_q, thread_nr):
         file_q.task_done()
     return True
 
+# Thread worker to read inventory and populate the file queue.
+# Each thread will read the content of a subfolder of source.
+def reader(folder_q, file_q, thread_nr):
+    while not folder_q.empty():
+        sub_folder = folder_q.get()
+        logging.info('Thread %d - reading sub-folder %s'
+             % (thread_nr, sub_folder))
+        for root, dirs, files in os.walk(sub_folder):
+            for i, filename in enumerate(files):
+                file_q.put(str(root + '/' + filename))
+        folder_q.task_done()
 
 if __name__ == "__main__":
     logging.basicConfig(filename='mtcopy.log', level=logging.DEBUG,
@@ -46,51 +65,60 @@ if __name__ == "__main__":
     logging.info('==== Starting new run ====')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("threads",
-        help="number of threads to be used when copying content")
+    parser.add_argument("threads", nargs='?', const=100, type=int,
+                    help="number of threads to be used when copying content")
     parser.add_argument("-s", "--source", required=True,
-        help="source folder, content of which will be copied")
+                    help="source folder, content of which will be copied")
     parser.add_argument("-d", "--destination", required=True,
-        help="specify full path to destination folder")
+                    help="specify full path to destination folder")
     args = parser.parse_args()
-    if not os.path.isdir(args.source):
+    dest_folder = os.path.abspath(args.destination)
+    source_folder = os.path.abspath(args.source)
+    if not os.path.isdir(source_folder):
         logging.error('The source path must be a folder.')
         sys.exit(1)
-    if not os.path.isdir(args.destination):
+    if not os.path.isdir(dest_folder):
         try:
-            os.mkdir(args.destination)
+            os.mkdir(dest_folder)
         except OSError:
-            logging.error("Creation of the directory %s failed, parent folder exists ?" % args.destination)
+            logging.error(
+                "Creation of the directory %s failed, parent folder exists ?"
+                % args.destination)
             sys.exit(1)
         else:
-            logging.info("Created the destination folder %s " % args.destination)
+            logging.info("Created the destination folder %s " % dest_folder)
     if int(args.threads) > MAX_THREADS:
-        logging.error('The number of threads must be maximum %d.' % MAX_THREADS)
+        logging.error('The number of threads must be maximum %d.' %
+                      MAX_THREADS)
         sys.exit(1)
-    dest_folder = os.path.abspath(args.destination) 
-    source_folder = os.path.abspath(args.source) 
     logging.info('Source folder: %s' % source_folder)
     logging.info('Destination folder: %s' % dest_folder)
     # define Queue and initializing threads
     file_q = Queue(maxsize=0)
+    folder_q = Queue(maxsize=0)
     count = 0
-    logging.info('Populating queue with filenames.')
-    for root, dirs, files in os.walk(source_folder):
-        for filename in files:
-            if count % 1000 == 0 and count > 0 :
-                logging.info('Queued %d items' % count)
-            file_q.put((count, str(root + '/' + filename)))
-            count += 1
-    # now we wait until the queue has been processed
-    logging.info('Queue populated with %d files.' % count)
-
+    logging.info('Populating queue.')
+    for f in os.listdir(source_folder):
+        folder_q.put(os.path.abspath(source_folder) + '/' + f)
+    logging.info('Starting threads to read the content of the %d subfolders.' \
+                 % folder_q.qsize())
+    for thread_nr in range(args.threads):
+        worker = Thread(target=reader, args=(folder_q, file_q, thread_nr))
+        worker.setDaemon(True)
+        worker.start()
+    folder_q.join()
+    logging.info('Queue populated with %d items.' % file_q.qsize())
+    size = file_q.qsize()
+    files_q = Queue(maxsize=0)
+    for i in range(size):
+        files_q.put((i,file_q.get()))
     for i in range(int(args.threads)):
         logging.info('Starting thread %d ' % i)
-        worker = Thread(target=copier, args=(file_q, i))
+        worker = Thread(target=copier, args=(files_q, i))
         worker.setDaemon(True)
-        worker.start()  
+        worker.start()
     logging.info('Threads started, waiting to complete.')
-    file_q.join()
-    if ERROR :
+    files_q.join()
+    if ERROR:
         logging.error("Errors occurred during copy - please check the logs.")
-    logging.info('All tasks completed, processed %d files.' % count)
+    logging.info('All tasks completed, processed %d files.' % size)
